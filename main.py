8345c2f51234fa534fbca789b63c29e91cc459c3
chahak1707 <pahujamanni1978@gmail.com>
2025-05-07 20:03:18 +0530

    Lesson 2

---
+++ main.py
@@ -0,0 +1,22 @@
+print("I am studying coding")
+print(17)
+print("Hello",10)
+print("*")
+print("Enter your name \n")
+name01 = "Chahak Pahuja"
+print(name01)
+
+adderess12  = "Abc uttam nagar, new delhi"
+print(adderess12)
+
+phone_number = 987123564
+print(phone_number)
+
+import keyword
+print(keyword.kwlist
+
+
+
+
+
+
